5403425cacf5a3d7e6a87f2f240af17ad35e56e7
Andre <andreandre_1234@hotmail.com>
2020-04-14 14:17:50 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('jquery');
+
+global.$ = global.jQuery = require('jquery');
+require('./bootstrap')
+require('bootstrap');
+require('jquery-easing');
+
+require('./jqBootstrapValidation');
+require('./contact_me');
+require('./agency.min.js')
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('content')
+    @include('templates.nav')
+    @include('templates.header')
+    @include('templates.services')
+    @include('templates.portfolio')
+    @include('templates.about')
+    @include('templates.team')
+    @include('templates.clients')
+    @include('templates.contact')
+    @include('templates.footer')
+    @include('templates.modals')
+@endsection
